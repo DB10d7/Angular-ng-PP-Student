525473e351c47aca12bdca0b877a42bf4c364c71
piofxdev1 <piofxdev1@gmail.com>
2021-10-05 16:32:30 +0530

    register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterSuccessComponent } from './auth/register-success/register-success.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:"register", component: RegisterComponent
+  },
+  {
+    path:"register-success", component: RegisterSuccessComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
